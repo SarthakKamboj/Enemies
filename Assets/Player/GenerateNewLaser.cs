d4a202fb78aed00333486fa70d1ae911198f5904
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-06-13 11:06:43 -0700

    added creating new lasers

--- Assets/Player/GenerateNewLaser.cs
+++ Assets/Player/GenerateNewLaser.cs
@@ -7,11 +7,25 @@ public class GenerateNewLaser : MonoBehaviour
 	[SerializeField] Transform _ground;
 
 	DisposeEnemy _disposeEnemy;
+	Vector3 _groundExtents;
+	Vector3 _laserExtents;
+	Detector _laserDetector;
 
 	void Awake()
 	{
 		_disposeEnemy = GetComponent<DisposeEnemy>();
 		_disposeEnemy.AddEnemyDropOffListener(CreateNewLaser);
+		_groundExtents = _ground.GetComponent<Collider>().bounds.extents;
+		_laserDetector = GetComponent<Detector>();
+		_laserExtents = GetLaserExents();
+	}
+
+	Vector3 GetLaserExents()
+	{
+		GameObject tempLaser = Instantiate(_laserPrefab);
+		Vector3 laserExtents = tempLaser.GetComponent<Collider>().bounds.extents;
+		Destroy(tempLaser);
+		return laserExtents;
 	}
 
 	void OnDestroy()
@@ -26,7 +40,19 @@ public class GenerateNewLaser : MonoBehaviour
 
 	Vector3 GenerateRandomPoint()
 	{
-		return Vector3.zero;
+		float x, y, z;
+		Vector3 newPos;
+
+		y = _ground.position.y + _groundExtents.y + _laserExtents.y;
+		do
+		{
+			x = Random.Range(_ground.position.x - _groundExtents.x, _ground.position.x + _groundExtents.x);
+			z = Random.Range(_ground.position.z - _groundExtents.z, _ground.position.z + _groundExtents.z);
+			newPos = new Vector3(x, y, z);
+		} while (_laserDetector.Detect(newPos));
+
+		return newPos;
 	}
+
 
 }
