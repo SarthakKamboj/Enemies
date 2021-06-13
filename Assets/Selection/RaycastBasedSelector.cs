57e0d724ce96feda45813cbd5b078747d05197e2
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-06-13 02:43:00 -0700

    made raycast infinite distance

--- Assets/Selection/RaycastBasedSelector.cs
+++ Assets/Selection/RaycastBasedSelector.cs
@@ -6,28 +6,27 @@ public class RaycastBasedSelector : Selector
 	[SerializeField] LayerMask _layerMask;
 
 	RaycastHit _hitInfo;
-	Transform _hitTransform;
 
 	public override void CheckRay(Ray ray)
 	{
-		if (Physics.Raycast(ray, out var hit, 20f, _layerMask))
+		if (Physics.Raycast(ray, out var hit, Mathf.Infinity, _layerMask))
 		{
 			_hitInfo = hit;
-			_hitTransform = _hitInfo.transform;
 		}
 		else
 		{
-			_hitTransform = null;
+			_hitInfo = new RaycastHit();
 		}
 	}
 
 	public override Transform GetSelection()
 	{
-		return _hitTransform;
+		return _hitInfo.transform;
 	}
 
 	public override Vector3 GetSelectionPoint()
 	{
 		return _hitInfo.point;
 	}
+
 }
