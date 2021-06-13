78a26a6aaef5cee627cb992f87b81050871f54a8
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-06-12 20:35:25 -0700

    added enemy ai

---
+++ Assets/Enemy/EnemyController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+
+	[SerializeField] NavMeshAgent _agent;
+	[SerializeField] Transform _player;
+
+	Transform _t;
+
+	void Start()
+	{
+		_t = transform;
+	}
+
+	void Update()
+	{
+		Vector3 lookDir = (_player.position - _t.position).normalized;
+		_t.rotation = Quaternion.LookRotation(lookDir, Vector3.up);
+		_agent.SetDestination(_player.position);
+	}
+}
