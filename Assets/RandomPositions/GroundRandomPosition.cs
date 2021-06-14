9d4c2770d439f3cc10feacfca95c009337a78171
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-06-14 00:58:00 -0700

    refactored random pos generation on ground

---
+++ Assets/RandomPositions/GroundRandomPosition.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class GroundRandomPosition : RandomPosition
+{
+
+	[SerializeField] Detector _invalidLocationDetector;
+
+	Transform _transform;
+	Vector3 _groundExtents;
+
+	void Awake()
+	{
+		_transform = transform;
+		_groundExtents = _transform.GetComponent<Collider>().bounds.extents;
+	}
+
+	public override Vector3 GetRandomPosition()
+	{
+		float x, y, z;
+		Vector3 newPos;
+
+		y = _transform.position.y + _groundExtents.y;
+		do
+		{
+			x = Random.Range(_transform.position.x - _groundExtents.x, _transform.position.x + _groundExtents.x);
+			z = Random.Range(_transform.position.z - _groundExtents.z, _transform.position.z + _groundExtents.z);
+			newPos = new Vector3(x, y, z);
+		} while (_invalidLocationDetector.Detect(newPos));
+
+		return newPos;
+	}
+
+}
---
+++ Assets/RandomPositions/RandomPosition.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class RandomPosition : MonoBehaviour
+{
+
+	public virtual Vector3 GetRandomPosition()
+	{
+		throw new NotImplementedException();
+	}
+
+}
