e210c77d1cab1b73ddd44aeece2231a30f48547f
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-06-14 00:58:28 -0700

    added laser model

--- Assets/Laser/LaserActivationManager.cs
+++ Assets/Laser/LaserActivationManager.cs
@@ -4,19 +4,23 @@ using UnityEngine;
 public class LaserActivationManager : MonoBehaviour
 {
 
-	[SerializeField] Material activeMaterial;
 	[SerializeField] Material inActiveMaterial;
 	[SerializeField] float _timeUntilReactivation = 5f;
 
 	bool deactivated = false;
 	Renderer _renderer;
 	LaserKillEnemy _killEnemy;
+	Material _activeMaterial;
+	static bool once = true;
 
 	void Awake()
 	{
 		_renderer = transform.Find("GFX").GetComponent<Renderer>();
 		_killEnemy = GetComponent<LaserKillEnemy>();
 		_killEnemy.AddOnEnemyKilledListener(Deactivate);
+
+		_activeMaterial = _renderer.material;
+
 	}
 
 	void OnDestroy()
@@ -35,7 +39,7 @@ public class LaserActivationManager : MonoBehaviour
 	{
 		yield return new WaitForSeconds(_timeUntilReactivation);
 		deactivated = false;
-		_renderer.material = activeMaterial;
+		_renderer.material = _activeMaterial;
 	}
 
 	public bool IsLaserDeactivated()
