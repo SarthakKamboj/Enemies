912d296e83d2224e88e65e3933116615343b3229
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-06-16 01:06:08 -0700

    player can now pick up enemy die particle by colliding with it

--- Assets/Player/PickUpEnemy.cs
+++ Assets/Player/PickUpEnemy.cs
@@ -4,29 +4,38 @@ public class PickUpEnemy : MonoBehaviour
 {
 
 	[SerializeField] Selector _deadEnemyParticlePickUpSelector;
+	[SerializeField] LayerMask _deadEnemyParticleLayerMask;
 
 	RayProvider _screenRayProvider;
 	Storage _enemyStorage;
+	const float WAIT_TIME = 1f;
+	float _waitTime;
 
 	void Awake()
 	{
 		_screenRayProvider = GetComponent<RayProvider>();
 		_enemyStorage = GetComponent<Storage>();
+		_waitTime = 0f;
 	}
 
 	void Update()
 	{
-		if (Input.GetMouseButtonDown(0))
+		if (AimedAtEnemyParticle())
 		{
-			Ray ray = _screenRayProvider.GetRay();
-			_deadEnemyParticlePickUpSelector.CheckRay(ray);
-
 			Transform deadEnemyParticle = _deadEnemyParticlePickUpSelector.GetSelection();
-			if (deadEnemyParticle != null)
-			{
-				PickUp(deadEnemyParticle);
-			}
+			PickUp(deadEnemyParticle);
 		}
+
+		_waitTime = Mathf.Max(0f, _waitTime - Time.deltaTime);
+	}
+
+	bool AimedAtEnemyParticle()
+	{
+		Ray ray = _screenRayProvider.GetRay();
+		_deadEnemyParticlePickUpSelector.CheckRay(ray);
+
+		Transform deadEnemyParticle = _deadEnemyParticlePickUpSelector.GetSelection();
+		return Input.GetMouseButtonDown(0) && deadEnemyParticle != null;
 	}
 
 	void PickUp(Transform t)
@@ -35,5 +44,19 @@ public class PickUpEnemy : MonoBehaviour
 		_enemyStorage.AddToStorage(t);
 	}
 
+	void OnControllerColliderHit(ControllerColliderHit hit)
+	{
+		if (IsInLayerMask(_deadEnemyParticleLayerMask, hit.gameObject.layer) && _waitTime <= 0f)
+		{
+			Transform deadEnemyParticle = hit.transform;
+			PickUp(deadEnemyParticle);
+			_waitTime = WAIT_TIME;
+		}
+	}
+
+	bool IsInLayerMask(LayerMask layerMask, int layer)
+	{
+		return layerMask == (layerMask | (1 << layer));
+	}
 
 }
