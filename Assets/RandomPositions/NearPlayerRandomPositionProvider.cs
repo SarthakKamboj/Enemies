f25eebca0313e6e60f51ac7597724901318d7946
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-06-17 01:00:27 -0700

    generator for position near player

---
+++ Assets/RandomPositions/NearPlayerRandomPositionProvider.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class NearPlayerRandomPositionProvider : RandomPosition
+{
+
+	Transform _t;
+	[SerializeField] float _radius = 10f;
+
+	void Awake()
+	{
+		_t = transform;
+	}
+
+	public override Vector3 GetRandomPosition()
+	{
+		Vector3 offset = Random.insideUnitCircle * _radius;
+		return _t.position + offset;
+	}
+}
