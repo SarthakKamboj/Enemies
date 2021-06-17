f003d7161b90999a4b6723a304a6ee2a9fefce46
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-06-17 01:01:29 -0700

    made enemy go to random position near the player rather than player position

--- Assets/Enemy/EnemyController.cs
+++ Assets/Enemy/EnemyController.cs
@@ -7,11 +7,15 @@ public class EnemyController : MonoBehaviour
 	[SerializeField] NavMeshAgent _agent;
 	[SerializeField] TransformScrObj _playerObj;
 	[SerializeField] Animator _enemyAnimator;
+	[SerializeField] float TimeBetweenEnemyMove = 5f;
 
 	Transform _t;
 	EnemyDie _baseEnemyDie;
 	Transform _player;
 	int speedHash;
+	Vector3 _targetPos;
+	float _timeUntilEnemyMove;
+	RandomPosition _nearPlayerRandomPositionProvider;
 
 
 	void Awake()
@@ -22,11 +26,15 @@ public class EnemyController : MonoBehaviour
 
 		_playerObj.AddTransformChangeListener(UpdatePlayerRef);
 		speedHash = Animator.StringToHash("Speed");
+
+		_timeUntilEnemyMove = TimeBetweenEnemyMove;
 	}
 
 	void Start()
 	{
 		UpdatePlayerRef(_playerObj.GetTransform());
+		_nearPlayerRandomPositionProvider = _player.GetComponent<RandomPosition>();
+		GetNewTargetPosition();
 	}
 
 	void OnDestroy()
@@ -42,16 +50,36 @@ public class EnemyController : MonoBehaviour
 
 	void Update()
 	{
+
+		_timeUntilEnemyMove = Mathf.Max(_timeUntilEnemyMove - Time.deltaTime, 0f);
+
+		if (_timeUntilEnemyMove <= 0f)
+		{
+			_timeUntilEnemyMove = TimeBetweenEnemyMove;
+			GetNewTargetPosition();
+		}
+
 		Vector3 lookDir = (_player.position - _t.position).normalized;
 		_t.rotation = Quaternion.LookRotation(lookDir, Vector3.up);
-		_agent.SetDestination(_player.position);
+		_agent.SetDestination(_targetPos);
 		float normalizedAgentVel = _agent.velocity.magnitude / _agent.speed;
 		_enemyAnimator.SetFloat(speedHash, normalizedAgentVel);
+	}
+
+	void GetNewTargetPosition()
+	{
+		_targetPos = _nearPlayerRandomPositionProvider.GetRandomPosition();
 	}
 
 	void OnEnemyDie()
 	{
 		_agent.enabled = false;
 		this.enabled = false;
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawSphere(_targetPos, 5f);
 	}
 }
