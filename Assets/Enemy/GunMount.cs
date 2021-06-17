b74b7ebd543397fd208530b91fc32fca4c38efa9
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-06-16 23:12:33 -0700

    added gun and gun anims for masked enemy

---
+++ Assets/Enemy/GunMount.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class GunMount : MonoBehaviour
+{
+
+	[SerializeField] Transform _leftHand, _rightHand;
+	[SerializeField] Transform _gun;
+
+	void Update()
+	{
+		Vector3 pos = (_leftHand.position + _rightHand.position) / 2f;
+		_gun.position = pos;
+		Vector3 gunDir = _leftHand.position - _rightHand.position;
+		gunDir.y = Mathf.Abs(gunDir.y);
+		_gun.rotation = Quaternion.LookRotation(gunDir, Vector3.up);
+	}
+
+}
