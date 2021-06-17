5d17daf1830b077e2e7e7214d1a8838d7e1fc239
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-06-16 18:35:01 -0700

    added rot for enemy particle

---
+++ Assets/Misc/Rotate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+	[SerializeField] float _period;
+
+	void Update()
+	{
+		Debug.Log(_period * Time.deltaTime * 360f);
+		transform.Rotate(Vector3.up, Time.deltaTime * 360f / _period);
+	}
+}
