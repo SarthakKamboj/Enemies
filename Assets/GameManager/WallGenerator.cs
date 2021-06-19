4b3a0437f43a5776705274b2af23de214cd6ff96
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-06-19 16:17:38 -0700

    'procedurally generate walls

---
+++ Assets/GameManager/WallGenerator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class WallGenerator : MonoBehaviour
+{
+
+	[SerializeField] GameObject _wallPrefab;
+	[SerializeField] int _numWalls = 5;
+	[SerializeField] RandomPosition _groundRandomPosition;
+
+	Vector3 _wallExtents;
+
+	void Awake()
+	{
+		GameObject wall = Instantiate(_wallPrefab);
+		_wallExtents = wall.GetComponent<Collider>().bounds.extents;
+		Destroy(wall);
+	}
+
+	void Start()
+	{
+		for (int i = 0; i < _numWalls; i++)
+		{
+			Vector3 pos = _groundRandomPosition.GetRandomPosition() + new Vector3(0f, _wallExtents.y, 0f);
+			Quaternion quat = Quaternion.Euler(new Vector3(0f, Random.Range(0, 4) * 90f, 0f));
+			CreateWall(pos, quat);
+		}
+	}
+
+	void CreateWall(Vector3 pos, Quaternion quat)
+	{
+		Instantiate(_wallPrefab, pos, quat, null);
+	}
+
+}
