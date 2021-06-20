67eba4d8e85673eb41098fd62c79ce88d3a0b741
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-06-19 17:00:09 -0700

    changed from partial class to just class

--- Assets/Enemy/MoveBackAndForthState.cs
+++ Assets/Enemy/MoveBackAndForthState.cs
@@ -1,49 +1,46 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-public partial class EnemyTarget
+public class MoveBackAndForthState : State
 {
-	class MoveBackAndForthState : State
+	public Vector3 _forwardPos, _backPos;
+	Transform _t;
+	float _length;
+	float _stoppingDistance;
+	int _moveDir;
+	NavMeshAgent _agent;
+
+	public MoveBackAndForthState(Transform t, float length, NavMeshAgent agent, float stoppingDistance)
 	{
-		public Vector3 _forwardPos, _backPos;
-		Transform _t;
-		float _length;
-		float _stoppingDistance;
-		int _moveDir;
-		NavMeshAgent _agent;
+		_length = length;
+		_t = t;
+		_moveDir = 1;
+		_agent = agent;
+		_stoppingDistance = stoppingDistance;
+	}
 
-		public MoveBackAndForthState(Transform t, float length, NavMeshAgent agent, float stoppingDistance)
+	public override void Start()
+	{
+		UpdatePositions();
+		_agent.SetDestination(_forwardPos);
+	}
+
+	public override void Tick()
+	{
+		if (Vector3.Distance(_t.position, _forwardPos) <= _stoppingDistance)
 		{
-			_length = length;
-			_t = t;
-			_moveDir = 1;
-			_agent = agent;
-			_stoppingDistance = stoppingDistance;
+			_agent.SetDestination(_backPos);
 		}
-
-		public override void Start()
+		else if (Vector3.Distance(_t.position, _backPos) <= _stoppingDistance)
 		{
-			UpdatePositions();
 			_agent.SetDestination(_forwardPos);
 		}
+	}
 
-		public override void Tick()
-		{
-			if (Vector3.Distance(_t.position, _forwardPos) <= _stoppingDistance)
-			{
-				_agent.SetDestination(_backPos);
-			}
-			else if (Vector3.Distance(_t.position, _backPos) <= _stoppingDistance)
-			{
-				_agent.SetDestination(_forwardPos);
-			}
-		}
-
-		void UpdatePositions()
-		{
-			this._forwardPos = _t.position + _t.forward * _length;
-			this._backPos = _t.position - _t.forward * _length;
-		}
+	void UpdatePositions()
+	{
+		this._forwardPos = _t.position + _t.forward * _length;
+		this._backPos = _t.position - _t.forward * _length;
 	}
 }
 
--- Assets/Enemy/TargetPlayer.cs
+++ Assets/Enemy/TargetPlayer.cs
@@ -1,30 +1,27 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-public partial class EnemyTarget
+public class TargetPlayer : State
 {
-	class TargetPlayer : State
+
+	Transform _player, _t;
+	NavMeshAgent _agent;
+	ShootBase _shoot;
+
+	public TargetPlayer(Transform player, NavMeshAgent agent, Transform t, ShootBase shoot)
 	{
+		_player = player;
+		_agent = agent;
+		_t = t;
+		_shoot = shoot;
+	}
 
-		Transform _player, _t;
-		NavMeshAgent _agent;
-		ShootBase _shoot;
-
-		public TargetPlayer(Transform player, NavMeshAgent agent, Transform t, ShootBase shoot)
-		{
-			_player = player;
-			_agent = agent;
-			_t = t;
-			_shoot = shoot;
-		}
-
-		public override void Tick()
-		{
-			Vector3 lookDir = (_player.position - _t.position).normalized;
-			_t.rotation = Quaternion.LookRotation(lookDir, Vector3.up);
-			_shoot.Shoot();
-			_agent.SetDestination(_player.position);
-		}
+	public override void Tick()
+	{
+		Vector3 lookDir = (_player.position - _t.position).normalized;
+		_t.rotation = Quaternion.LookRotation(lookDir, Vector3.up);
+		_shoot.Shoot();
+		_agent.SetDestination(_player.position);
 	}
 }
 
