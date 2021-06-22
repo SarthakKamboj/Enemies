e952cc7d34ecfd7e45da657e43a9f2b1c7cb6703
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-06-22 01:58:38 -0700

    made relative target rotation better

--- Assets/Enemies/Scripts/FollowEntity.cs
+++ Assets/Enemies/Scripts/FollowEntity.cs
@@ -1,23 +1,44 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-public class FollowEntity : State
+public class FollowEntity : State, ITransform
 {
 	Transform _target, _t;
 	NavMeshAgent _agent;
+	float _vicinityDistance;
+	float _curVel;
 
-	public FollowEntity(Transform target, NavMeshAgent agent, Transform t)
+	public FollowEntity(Transform target, NavMeshAgent agent, Transform t, float vicinityDistance)
 	{
 		_target = target;
 		_agent = agent;
 		_t = t;
+		_vicinityDistance = vicinityDistance;
+	}
+
+	public Transform GetTransform()
+	{
+		return _target;
 	}
 
 	public override void Tick()
 	{
-		Vector3 lookDir = (_target.position - _t.position).normalized;
-		// _t.rotation = Quaternion.LookRotation(lookDir, Vector3.up);
-		_t.rotation = _target.rotation;
+		Vector3 diff = _target.position - _t.position;
+		Quaternion targetQuat;
+		if (diff.magnitude > _vicinityDistance)
+		{
+			Vector3 lookDir = diff.normalized;
+			targetQuat = Quaternion.LookRotation(lookDir, Vector3.up);
+		}
+		else
+		{
+			targetQuat = _target.rotation;
+		}
+
+		float targetYRot = targetQuat.eulerAngles.y;
+		float yRot = Mathf.SmoothDampAngle(_t.eulerAngles.y, targetYRot, ref _curVel, 0.2f);
+		_t.rotation = Quaternion.Euler(new Vector3(0f, yRot, 0f));
+
 		_agent.SetDestination(_target.position);
 	}
 }
