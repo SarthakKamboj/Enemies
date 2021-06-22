a48118f10b91e40ab7c3a128d5614419fc1acaa6
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-06-21 18:50:16 -0700

    created base script to convert enemy to player side

---
+++ Assets/Converter/Scripts/ConvertEnemyToPlayerSide.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ConvertEnemyToPlayerSide : MonoBehaviour
+{
+
+	[SerializeField] LayerMask _enemyLayer;
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (LayerMaskUtils.IsInLayer(_enemyLayer, collider.gameObject.layer))
+		{
+			Debug.Log("hit");
+		}
+	}
+
+}
