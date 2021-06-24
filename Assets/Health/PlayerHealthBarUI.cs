a1d9e35089ce464188789bd389fa4366cb897cac
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-06-23 17:42:37 -0700

    added player healthbar

---
+++ Assets/Health/PlayerHealthBarUI.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerHealthBarUI : MonoBehaviour
+{
+
+	[SerializeField] FloatScrObj _playerHealthObj;
+	[SerializeField] Transform _healthContainer;
+
+	float StartHealth = -1f;
+	float maxScale = -1f;
+
+	void Awake()
+	{
+		_playerHealthObj.AddChangeListener(UpdateUI);
+	}
+
+	void OnDestroy()
+	{
+		_playerHealthObj.AddChangeListener(UpdateUI);
+	}
+
+	void UpdateUI(float health)
+	{
+		if (StartHealth == -1f)
+		{
+			StartHealth = health;
+		}
+
+		Vector3 scale = _healthContainer.localScale;
+		scale.x = health / StartHealth;
+		_healthContainer.localScale = scale;
+
+	}
+
+	void Update()
+	{
+
+	}
+}
