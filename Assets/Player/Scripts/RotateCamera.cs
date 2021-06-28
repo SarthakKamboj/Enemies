689d85e11e8e9b89d975726554080d040830be0b
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-06-28 15:37:45 -0700

    added cam rotation

---
+++ Assets/Player/Scripts/RotateCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+
+	[SerializeField] Transform _camera;
+
+	void Update()
+	{
+		if (Input.GetMouseButton(0))
+		{
+			float horizontal = Input.GetAxisRaw("Mouse X");
+			Vector3 angles = _camera.eulerAngles;
+			angles.y += horizontal;
+			_camera.eulerAngles = angles;
+		}
+	}
+}
